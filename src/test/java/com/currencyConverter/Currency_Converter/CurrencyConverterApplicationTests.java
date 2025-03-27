f436aedcbff1bbd310951482474b11162798a268
Anshuman <chaoanxuman999@gmail.com>
2025-03-28 00:44:31 +0530

    Currency Converter

---
+++ src/test/java/com/currencyConverter/Currency_Converter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencyConverter.Currency_Converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
